ce74d131213c15cf88f01dc1abb87ba0bfc2b587
alexcoder04 <alexcoder04@protonmail.com>
2022-05-05 22:31:19 +0200

    external commands in modules

--- main.go
+++ main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os/exec"
+	"strings"
 	"time"
 )
+
+type Module struct {
+	Text            string   `json:"full_text"`
+	ForegroundColor string   `json:"color"`
+	BackgroundColor string   `json:"background"`
+	Command         string   `json:"-"`
+	Args            []string `json:"-"`
+}
+
+func UpdateModule(module Module) Module {
+	cmd := exec.Command(module.Command, module.Args...)
+	out, err := cmd.Output()
+	if err != nil {
+		module.Text = "error"
+	} else {
+		module.Text = strings.Replace(string(out), "\n", " ", -1)
+	}
+	return module
+}
 
 func main() {
 	fmt.Println(`{"version": 1, "click_events": true}`)
 	fmt.Println("[")
+
+	modules := []Module{
+		{"", "#0000ff", "#ffff00", "echo", []string{"kherson"}},
+		{"", "#ffffff", "#000000", "date", []string{"+%d.%m.%Y - %R:%S"}}}
+
 	i := 1
 	for {
-		fmt.Printf(`[{"full_text": "HELLO %d", "color": "#0000ff", "background": "#ffff00"}],`+"\n", i)
+		listJson := ""
+		for i := 0; i < len(modules); i++ {
+			modules[i] = UpdateModule(modules[i])
+			moduleJson, err := json.Marshal(modules[i])
+			if err != nil {
+				panic("error marshaling")
+			}
+			if listJson == "" {
+				listJson = "[" + string(moduleJson)
+				continue
+			}
+			listJson = listJson + "," + string(moduleJson)
+		}
+		listJson = listJson + "],\n"
+
+		fmt.Printf(listJson)
 		time.Sleep(1 * time.Second)
 		i += 1
 	}
