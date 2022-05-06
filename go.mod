6ed914bb0bdfda6744b83be9398a1d4d5492f9cb
alexcoder04 <alexcoder04@protonmail.com>
2022-05-06 21:03:30 +0200

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/alexcoder04/kherson
 
 go 1.18
+
+require gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -3,44 +3,77 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path"
 	"strings"
 	"time"
+
+	"gopkg.in/yaml.v3"
 )
 
 type Module struct {
-	Text            string   `json:"full_text"`
-	ForegroundColor string   `json:"color"`
-	BackgroundColor string   `json:"background"`
-	Command         string   `json:"-"`
-	Args            []string `json:"-"`
+	Text            string   `json:"full_text" yaml:"Text"`
+	ForegroundColor string   `json:"color" yaml:"ForegroundColor"`
+	BackgroundColor string   `json:"background" yaml:"BackgroundColor"`
+	Icon            string   `json:"-" yaml:"Icon"`
+	Command         string   `json:"-" yaml:"Command"`
+	Args            []string `json:"-" yaml:"Args"`
+}
+
+type Config struct {
+	Modules []Module `yaml:"Modules"`
+}
+
+func LoadConfig() Config {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		panic("cannot get config directory")
+	}
+	config := Config{}
+	configFile := path.Join(configDir, "kherson", "config.yml")
+	_, err = os.Stat(configFile)
+	if err != nil {
+		config.Modules = []Module{
+			{"", "#0000ff", "#ffff00", " ", "echo", []string{"kherson"}},
+			{"", "#ffffff", "#000000", " ", "date", []string{"+%d.%m.%Y - %R:%S"}}}
+		return config
+	}
+	data, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic("cannot read config file")
+	}
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic("error parsing config")
+	}
+	return config
 }
 
 func UpdateModule(module Module) Module {
 	cmd := exec.Command(module.Command, module.Args...)
 	out, err := cmd.Output()
 	if err != nil {
-		module.Text = "error"
+		module.Text = " error"
 	} else {
-		module.Text = strings.Replace(string(out), "\n", " ", -1)
+		module.Text = module.Icon + strings.Replace(string(out), "\n", " ", -1)
 	}
 	return module
 }
 
 func main() {
+	config := LoadConfig()
+
 	fmt.Println(`{"version": 1, "click_events": true}`)
 	fmt.Println("[")
-
-	modules := []Module{
-		{"", "#0000ff", "#ffff00", "echo", []string{"kherson"}},
-		{"", "#ffffff", "#000000", "date", []string{"+%d.%m.%Y - %R:%S"}}}
 
 	i := 1
 	for {
 		listJson := ""
-		for i := 0; i < len(modules); i++ {
-			modules[i] = UpdateModule(modules[i])
-			moduleJson, err := json.Marshal(modules[i])
+		for i := 0; i < len(config.Modules); i++ {
+			config.Modules[i] = UpdateModule(config.Modules[i])
+			moduleJson, err := json.Marshal(config.Modules[i])
 			if err != nil {
 				panic("error marshaling")
 			}
